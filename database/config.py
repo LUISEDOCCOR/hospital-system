b353b6981ec6461139e93e37022767d59b3fc9ce
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2025-03-22 21:02:30 -0600

    fix: configure_app

--- database/config.py
+++ database/config.py
@@ -4,6 +4,9 @@ from database.extensions import db
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'database.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+def configure_app ():
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'database.db')}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    return db
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from app import app
-from database.config import db
+from database.config import configure_app
+
+db = configure_app()
 
 if __name__ == "__main__":
     with app.app_context():
