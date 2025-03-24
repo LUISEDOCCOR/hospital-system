d5d8b5796811f0b82dceb93b1778003a11ec071d
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2025-03-24 11:44:53 -0600

    feat: Add the Tailwind CSS CLI, configure and add the main template and authentication page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.doctor_routes import bp as bp_doctor
 
 app = Flask(__name__)
@@ -7,3 +7,7 @@ app.register_blueprint(bp_doctor)
 @app.get("/ok")
 def health_check():
     return "ok"
+
+@app.get("/")
+def index():
+    return render_template("pages/auth.html")
